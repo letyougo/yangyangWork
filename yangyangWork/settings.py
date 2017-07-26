c1fb2063c78503f0b03d98ce5bbda69d4bdea481
surui1 <surui1@xiaomi.com>
2017-07-26 21:16:28 +0800

    sqlite to mysql

--- yangyangWork/settings.py
+++ yangyangWork/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'yangyangWork.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yangyang',
+        'USER': 'root',
+        'PASSWORD': 'surui123',
+        'HOST': '101.200.129.112',
+        'PORT': '3306',
     }
 }
 
